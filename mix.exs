3125155c36f40e6a7dfab4e3ad1d78d9b348fcb1
Christopher Lai <christopherliamlai@me.com>
2023-12-24 14:35:11 -0800

    Publish 2023.12.24-beta-1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Storyblok.MixProject do
   def project do
     [
       app: :storyblok,
-      version: "0.1.0-alpha-1",
+      version: "2023.12.24-beta-1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
