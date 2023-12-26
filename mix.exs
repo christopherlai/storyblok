37dc99b027d225adf702be31f4088a93982ce509
Christopher Lai <christopherliamlai@me.com>
2023-12-26 10:09:04 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Storyblok.MixProject do
   def project do
     [
       app: :storyblok,
-      version: "2023.12.24-beta.1",
+      version: "2023.12.24-beta.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
